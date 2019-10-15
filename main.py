0071e7b46b2f6491aa4f9d1e70d65baa30a2cca3
MadsVestergaard <45486626+MadsVestergaard@users.noreply.github.com>
2019-10-15 18:26:37 +0200

    Here we go lads

--- main.py
+++ main.py
@@ -8,12 +8,20 @@ from config import *
 def cvrapinavn(navn, land='dk'):
     """Looks up the the CVR-register of a company name and returns a dictionary from the API
 
-
-#asiduagsidugas
-
-
-
-
-
-
-# Hi there
+    :param navn: The name of the company searched for
+    :type file_loc: str
+    :param land: The country in which one wishes to search for the company in
+        (default is 'dk')
+    :type print_cols: str
+    :returns: A dictionary of the company, served from the API
+    :rtype: dict
+    """
+    response = requests.get(
+        url='http://cvrapi.dk/api',
+        params={'name': navn, 'country': land},
+        headers={'User-Agent': cvr_api_header})
+    if response:
+        return response.json()
+    else:
+        print("CVR API Error response:" + str(response))
+        raise TypeError("CVR-nummer findes ikke")
