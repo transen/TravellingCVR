81e46e04798b7abe605ba34e8fd1a8104bf1f8b2
Martin Broholt Trans <martin@broholttrans.dk>
2019-10-28 22:32:32 +0100

    Split long line

--- test_main.py
+++ test_main.py
@@ -53,7 +53,13 @@ from datetime import datetime
 
 testarg = "Transdesign.dk"
 
-testbusiness = {'vat': 38158686, 'name': 'Transdesign.dk', 'address': 'Langelandsgade 210, st. tv.', 'zipcode': '8200', 'city': 'Aarhus N', 'protected': False, 'phone': None, 'email': 'martin@transdesign.dk', 'startdate': '04/11 - 2016', 'employees': None, 'addressco': None, 'industrycode': 620100, 'industrydesc': 'Computerprogrammering', 'companycode': 10, 'companydesc': 'Enkeltmandsvirksomhed', 'creditbankrupt': False, 'owners': [{'name': 'Martin Broholt Trans'}], 'timeadded': datetime(2019, 10, 28, 22, 23, 34, 760576), 'status': 0, 'note': ''}
+testbusiness = {'vat': 38158686, 'name': 'Transdesign.dk', 'address': 'Langelandsgade 210, st. tv.',
+                'zipcode': '8200', 'city': 'Aarhus N', 'protected': False, 'phone': None,
+                'email': 'martin@transdesign.dk', 'startdate': '04/11 - 2016', 'employees': None, 'addressco': None,
+                'industrycode': 620100, 'industrydesc': 'Computerprogrammering', 'companycode': 10,
+                'companydesc': 'Enkeltmandsvirksomhed', 'creditbankrupt': False,
+                'owners': [{'name': 'Martin Broholt Trans'}], 'timeadded': datetime(2019, 10, 28, 22, 23, 34, 760576),
+                'status': 0, 'note': ''}
 
 
 def test_delete():
