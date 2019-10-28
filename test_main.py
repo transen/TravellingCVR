dd6d51e179ca0d16d68688af60382582200a5138
Martin Broholt Trans <martin@broholttrans.dk>
2019-10-28 22:28:17 +0100

    Rewrote tests to not query the inital API too much

--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 from mongofunctions import *
 from cvrapi import *
 from mapquestapi import *
+from datetime import datetime
 
 
 # def test(testarg="37158686"):
@@ -50,24 +51,28 @@ from mapquestapi import *
 #     insert_business(business)
 #     pull_single_business(testarg)
 
+testarg = "Transdesign.dk"
 
-def test_delete(testarg="Transdesign.dk"):
+testbusiness = {'vat': 38158686, 'name': 'Transdesign.dk', 'address': 'Langelandsgade 210, st. tv.', 'zipcode': '8200', 'city': 'Aarhus N', 'protected': False, 'phone': None, 'email': 'martin@transdesign.dk', 'startdate': '04/11 - 2016', 'employees': None, 'addressco': None, 'industrycode': 620100, 'industrydesc': 'Computerprogrammering', 'companycode': 10, 'companydesc': 'Enkeltmandsvirksomhed', 'creditbankrupt': False, 'owners': [{'name': 'Martin Broholt Trans'}], 'timeadded': datetime(2019, 10, 28, 22, 23, 34, 760576), 'status': 0, 'note': ''}
+
+
+def test_delete():
     delete_business(testarg)
 
 
-def test_business_from_api(testarg="Transdesign.dk"):
+def test_business_from_api():
     business_from_api(testarg)
 
 
-def test_attach_coords(testarg="Transdesign.dk"):
-    attach_coords(business_from_api(testarg))
+def test_attach_coords():
+    attach_coords(testbusiness)
 
 
-def test_insert_business(testarg="Transdesign.dk"):
-    insert_business(attach_coords(business_from_api(testarg)))
+def test_insert_business():
+    insert_business(testbusiness)
 
 
-def test_pull_one(testarg="Transdesign.dk"):
+def test_pull_one():
     pull_single_business(testarg)
 
 
