425cc237cc20754d073669944e04bdada1cd38df
Martin Broholt Trans <martin@broholttrans.dk>
2019-11-11 16:14:27 +0100

    Fixed error in appfunctions.py Added travisCI Added requirements.txt

---
+++ config.py
@@ -0,0 +1,12 @@
+# Secret keys, API-keys and everything else important and secret goes here
+
+secret_app_key = b'\xcb<\xdbEV\x8b\x04\x9dE\xac\xca\x18\x8b\xaa~\xbc'
+
+api_mapkey = '***REMOVED***'
+
+cvr_api_header = '***REMOVED***'
+
+mongo_user = '***REMOVED***'
+mongo_passw = '***REMOVED***'
+mongoclientstring = 'mongodb+srv://***REMOVED***:***REMOVED***@cvrdataapi-7bhwg.gcp.mongodb.net/travellingcvr' \
+***REMOVED***'
