6a349ac35caca49274ce3e7c46db4fbc8eea2537
Martin Broholt Trans <martin@broholttrans.dk>
2019-11-19 08:49:53 +0100

    Added case-insensitivity to user-system, with username and email Rewrote test to reflect changes

--- test_main.py
+++ test_main.py
@@ -66,7 +66,7 @@ def test_change_note():
 
 
 def test_pull_user():
-    assert pull_user("Martin")["email"] == "martin@broholttrans.dk"
+    assert pull_user("mArtin")["email"] == "martin@broholttrans.dk"
 
 
 def test_db_search():
