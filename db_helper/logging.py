b50030e60a1921dded1f2f8ea3011c62e01badb8
Martin Broholt Trans <martin@broholttrans.dk>
2019-12-02 12:32:49 +0100

    Documentation + cleanup in dependencies

--- db_helper/logging.py
+++ db_helper/logging.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
-from pymongo.errors import DuplicateKeyError
 from api_helpers.cvrapi import *
 from datetime import datetime
 
@@ -8,6 +7,21 @@ db = MongoClient(mongoclientstring).travellingcvr  # mongoclientstring hidden in
 
 
 def add_errorlog(actor=None, action=None, error=None):
+    """
+    This functions adds error-logging-capabilites for the webapp. Whenever the functions is called, it logs the current
+    time, the actor (who did something), the action (what that person did), and the error (what went wrong), from the
+    errors raised down through the stack. All the parameters are optional, and will default to None if nothing is
+    passed. It then inserts the error into the error-log-database.
+
+
+    :param actor: Who did something
+    :type actor: str
+    :param action: What someone did
+    :type action: str
+    :param error: What error occurred
+    :type error: str
+    :return: True
+    """
     now = datetime.now()
     error_element = {
         "time": now,
@@ -16,9 +30,24 @@ def add_errorlog(actor=None, action=None, error=None):
         "error": error
     }
     db.errorlog.insert_one(error_element)
+    return True
 
 
 def add_applog(actor=None, action=None, result=None):
+    """
+    This functions adds error-logging-capabilites for the webapp. Whenever the functions is called, it logs the current
+    time, the actor (who did something), the action (what that person did), and the result (what has changed).
+    All the parameters are optional, and will default to None if nothing is passed.  It then inserts the error into
+    the app-log-database.
+
+    :param actor: Who did something
+    :type actor: str
+    :param action: What someone did
+    :type action: str
+    :param result: What has changed
+    :type result: str
+    :return: True
+    """
     now = datetime.now()
     app_element = {
         "time": now,
@@ -27,3 +56,5 @@ def add_applog(actor=None, action=None, result=None):
         "result": result
     }
     db.applog.insert_one(app_element)
+    return True
+
