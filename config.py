e36da990a582441316b6c0996cb3680de5a0fe46
Martin Broholt Trans <martin@broholttrans.dk>
2019-12-10 11:23:48 +0100

    Added Sphinx documentation

--- config.py
+++ config.py
@@ -1,12 +1,20 @@
 # Secret keys, API-keys and everything else important and secret goes here
 
+#: The secret key is needed to keep the client-side sessions secure on the flask-app
 secret_app_key = b'\xcb<\xdbEV\x8b\x04\x9dE\xac\xca\x18\x8b\xaa~\xbc'
 
+#: API-key for MapQuest API
 api_mapkey = '***REMOVED***'
 
+#: The HTTP-header sent to the CVR-API
 cvr_api_header = '***REMOVED***'
 
+#: Username for the external MongoDB
 mongo_user = '***REMOVED***'
+
+#: Password for the external MongoDB
 mongo_passw = '***REMOVED***'
+
+#: The client-string to access the external MongoDB
 mongoclientstring = 'mongodb+srv://***REMOVED***:***REMOVED***@cvrdataapi-7bhwg.gcp.mongodb.net/travellingcvr' \
 ***REMOVED***'
